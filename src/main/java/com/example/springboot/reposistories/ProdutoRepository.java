de9420b7f7b516237c1b86705407207ca496a418
Daniel K I Ikeuchi <daniel.ikeuchi@gmail.com>
2022-06-02 19:45:52 -0300

    Branch main

---
+++ src/main/java/com/example/springboot/reposistories/ProdutoRepository.java
@@ -0,0 +1,11 @@
+package com.example.springboot.reposistories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.springboot.domain.Produto;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+
+}
