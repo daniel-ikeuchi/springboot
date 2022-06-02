7a5e290c5a5abf3001c8bb2f6a70e7fa99c9a2ca
Daniel K I Ikeuchi <daniel.ikeuchi@gmail.com>
2022-06-02 20:32:37 -0300

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/example/springboot/services/CategoriaService.java
+++ src/main/java/com/example/springboot/services/CategoriaService.java
@@ -7,16 +7,18 @@ import org.springframework.stereotype.Service;
 
 import com.example.springboot.domain.Categoria;
 import com.example.springboot.reposistories.CategoriaRepository;
+import com.example.springboot.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
 
 	@Autowired
 	private CategoriaRepository repo;
-	
+
 	public Categoria findById(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + " - Tipo: " + Categoria.class.getName()));
 	}
-	
+
 }
